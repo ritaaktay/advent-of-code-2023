d23d450c1432489f75da6a11889bf17d8aa0ab3b
ritaaktay <rita.aktay@vodaofone.com>
2023-12-24 15:01:32 +0000

    Day 2 Part 2 optimised

--- day-6/solution.test.js
+++ day-6/solution.test.js
@@ -5,38 +5,20 @@ const { input2 } = require("./input-2");
 
 // Solutions
 
-/* 
-{ time, distance}
-Winning option needs to be greater than distance in time allowed
-Get the multiplication of total number of winning options per race
-
-Ex. 
-{ time: 7, distance: 9 },
-Hold 1, (7 - 1) * 1 = 6
-Hold 2, (7 - 2) * 2 = 10
-Hold 3, (7 - 3 ) * 3 = 12
-Hold 4, (7 - 4) * 4 = 12
-Hold 5, (7 - 5) * 5 = 10
-Hold 6, (7 - 6) * 6 = 6 
-
-Brute force approach =>
-Look at each option (for all seconds from 1 to time - 1) and count
-
-More optimal approach => 
-(It's symettrical, so we can just look at the first half)
-
-Find at least => Bit where it goes above the distance needed (ex. 2, 10)
-Find no more than => Bit where it drops back below (ex. 5, 10)
-Find the difference 
-*/
+const getLowerLimit = (race) => {
+  for (let s = 1; s < race.time; s++) {
+    const distance = (race.time - s) * s;
+    if (distance > race.distance) {
+      return s;
+    }
+  }
+};
 
 const getRaceOptions = (race) => {
-  let count = 0;
-  for (let s = 1; s < race.time; s++) {
-    distance = (race.time - s) * s;
-    if (distance > race.distance) count++;
-  }
-  return count;
+  const lower = getLowerLimit(race);
+  const mid = race.time / 2;
+  const upper = mid + (mid - lower);
+  return upper - lower + 1;
 };
 
 const multiplyRaceOptions = (races) => {
@@ -59,5 +41,33 @@ describe("solution", () => {
 
 // Answers
 
-console.log("Part 1:", multiplyRaceOptions(input1));
-console.log("Part 2:", getRaceOptions(input2));
+console.log("Part 1:", multiplyRaceOptions(input1)); // 220320
+
+console.log("Part 2:", getRaceOptions(input2)); // 34454850
+
+/* 
+race: { time, distance}
+Winning option needs to be greater than distance in time allowed
+
+Ex. 
+{ time: 7, distance: 9 },
+Hold 0, (7 - 0) * 0 = 0
+Hold 1, (7 - 1) * 1 = 6
+Hold 2, (7 - 2) * 2 = 10
+Hold 3, (7 - 3 ) * 3 = 12
+Hold 4, (7 - 4) * 4 = 12
+Hold 5, (7 - 5) * 5 = 10
+Hold 6, (7 - 6) * 6 = 6 
+Hold 7, (7 - 7) * 7 = 0
+
+Brute force approach => approx. 20 seconds
+Look at each option (for all seconds from 1 to time - 1) 
+and count winning options
+
+More optimal approaches => approx. 0.2 seconds (100x faster)
+It's symettrical, so we can just look at the first half
+Find lower limit (via loop), find upper limit (via symmetry)
+Take their difference (plus +1 because limits are inclusive)
+The same symmetry applies to both even & odd time limit:
+mid + ( mid - lower limit ) = upper limit
+*/
